8c5a3eea7e308857b360b8079ef845862cea85b5
suvigyabhardwaj <34797153+suvigyabhardwaj@users.noreply.github.com>
2020-12-16 01:09:58 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
-st.write(""" # Simple Iris Flower Prediction App
+st.write(""" # Iris Flower Prediction App
 This app predicts the **Iris flower** type!
 """)
 
